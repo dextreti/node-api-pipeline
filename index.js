b1bc11e6a17fc3a374cbea40ab501e160440e34a
dextreti <dextreti@gmail.com>
2026-02-07 08:49:26 -0800

    fix: events

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 
-async function getStatus() {
-   let x = 10;
-    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
-    
-    if (x === x) { // Bug: Comparación idéntica (siempre es true)
-        return "esto es un error de lógica en getStatus";
-    }
-}
 
 // BUG PARA FORZAR FALLO EN JENKINS
 function testingBugs() {
