004c72a21e280c73d5c9f8f09d36b371d08c23d7
dextreti <dextreti@gmail.com>
2026-02-07 10:02:45 -0800

    fix: branch main

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
+function saveBugs() {
+    let x = 10;
+    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
+    
+    if (x === x) { // Bug: Comparación idéntica (siempre es true)
+        return "esto es un error de lógica";
+    }
+}
 
 // BUG PARA FORZAR FALLO EN JENKINS
 function testingBugs() {
