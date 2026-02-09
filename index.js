0fc4365392af68a1e6b766b0fa9571a32379573b
dextreti <dextreti@gmail.com>
2026-02-09 13:25:38 -0800

    test:slack me

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-function noticeBugs() {
-    let x = 10;
-    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
-    
-    if (x === x) { // Bug: Comparación idéntica (siempre es true)
-        return "esto es un error de lógica";
-    }
-}
-
 function saveBugs() {
     let x = 10;
     x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
