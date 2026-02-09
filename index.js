3aeb2be3636cf801f951067dc9202cae0c537871
dextreti <dextreti@gmail.com>
2026-02-09 13:26:27 -0800

    test:slack mesage

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-function saveBugs() {
+function saveBug() {
     let x = 10;
     x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
     
