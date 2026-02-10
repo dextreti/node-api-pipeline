2f709498bc8da3603322fbdc62434d1e468b370c
dextreti <dextreti@gmail.com>
2026-02-10 14:53:59 -0800

    Fix: test scenary success

--- index.js
+++ index.js
@@ -7,35 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-function Bugs() {
-    let x = 10;
-    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
-    
-    if (x === x) { // Bug: Comparación idéntica (siempre es true)
-        return "esto es un error de lógica";
-    }
-}
-
-
-function sendBug() {
-    let x = 10;
-    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
-    
-    if (x === x) { // Bug: Comparación idéntica (siempre es true)
-        return "esto es un error de lógica";
-    }
-}
-
-// BUG PARA FORZAR FALLO EN JENKINS
-function testingBugs() {
-    let x = 10;
-    x = 20; // Dead store: Sonar dirá "para qué asignas 10 si luego pones 20"
-    
-    if (x === x) { // Bug: Comparación idéntica (siempre es true)
-        return "esto es un error de lógica";
-    }
-}
-
 // Endpoint para probar la conexión con Northwind
 app.get('/products', async (req, res) => {
   try {
