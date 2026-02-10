647eef5d9ee4035a999414ef0d796be895d34d7e
dextreti <dextreti@gmail.com>
2026-02-10 15:49:04 -0800

    test functon

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-function Bugsing() {
-    let x = 10;
-    x = 20; 
-    
-    if (x === x) { 
-        return "esto es un error de lógica";
-    }
-}
-
 // Endpoint para probar la conexión con Northwind
 app.get('/products', async (req, res) => {
   try {
