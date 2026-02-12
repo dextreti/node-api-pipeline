06c2657a6a9ab1ef45bd5c0265cc3402c6ee0d57
dextreti <dextreti@gmail.com>
2026-02-12 07:24:39 -0800

    4 rows

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/products', async (req, res) => {
   try {
     const products = await prisma.products.findMany({
-      take: 8, 
+      take: 4, 
       select: {
         product_id: true,
         product_name: true,
