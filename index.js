297eff002f3e1e5359fa943007483d69d81b74fc
dextreti <dextreti@gmail.com>
2026-02-12 06:51:56 -0800

    changed limit rows

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/products', async (req, res) => {
   try {
     const products = await prisma.products.findMany({
-      take: 10, 
+      take: 5, 
       select: {
         product_id: true,
         product_name: true,
