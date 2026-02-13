30cf61e9f3cc34ffe739adfc2de22848086892a4
dextreti <dextreti@gmail.com>
2026-02-13 09:33:27 -0800

    FIX:12

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/products', async (req, res) => {
   try {
     const products = await prisma.products.findMany({
-      take: 4, 
+      take: 14, 
       select: {
         product_id: true,
         product_name: true,
